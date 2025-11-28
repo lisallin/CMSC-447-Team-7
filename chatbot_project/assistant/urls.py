a1dbb92c0235450654864c46172ad7951e1696a4
ebdt <emalik1@umbc.edu>
2025-11-28 13:30:45 -0500

    pushing old backend code to update

--- chatbot_project/assistant/urls.py
+++ chatbot_project/assistant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin # added my eba
 from . import views
 
 urlpatterns = [
     path('faqs/', views.faq_list, name='faq_list'),
+    path('admin/', admin.site.urls),  # Django admin at /admin/
     
 ]
